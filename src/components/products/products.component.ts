d8a8459cdccbb4c04aeed8b6fd53d91d38093360
AARYANS-POWERHO\Aaryan Jain <aaryan.vijayraj@gmail.com>
2024-09-28 22:49:02 +0530

    added a products component - it can be changed later

--- src/components/products/products.component.ts
+++ src/components/products/products.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
-  selector: 'app-products',
-  standalone: true,
-  imports: [],
-  templateUrl: './products.component.html',
-  styleUrl: './products.component.scss',
+    selector: 'app-products',
+    standalone: true,
+    imports: [MatCardModule],
+    templateUrl: './products.component.html',
+    styleUrl: './products.component.scss',
 })
 export class ProductsComponent {}
